b21b4fbc78547dae529bfb85034c94bbb09ca169
tlarbals824 <simkm0824@naver.com>
2023-12-15 18:47:51 +0900

    refactor : TodoService 코틀린 내장 함수 이용 리팩터링

--- TodoJava/src/main/java/com/fastcampus/kotlinspring/todo/service/TodoService.kt
+++ TodoJava/src/main/java/com/fastcampus/kotlinspring/todo/service/TodoService.kt
@@ -4,6 +4,7 @@ import com.fastcampus.kotlinspring.todo.api.model.TodoRequest
 import com.fastcampus.kotlinspring.todo.domain.Todo
 import com.fastcampus.kotlinspring.todo.domain.TodoRepository
 import org.springframework.data.domain.Sort
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -15,35 +16,36 @@ class TodoService(
     private val todoRepository: TodoRepository
 ) {
     @Transactional(readOnly = true)
-    fun findAll() = todoRepository.findAll(Sort.by(Sort.Direction.DESC, "id"))
+    fun findAll(): List<Todo> = todoRepository.findAll(Sort.by(Sort.Direction.DESC, "id"))
 
     @Transactional(readOnly = true)
-    fun findById(id: Long) = todoRepository.findById(id).orElseThrow { ResponseStatusException(HttpStatus.NOT_FOUND) }
+    fun findById(id: Long): Todo =
+        todoRepository.findByIdOrNull(id) ?: throw ResponseStatusException(HttpStatus.NOT_FOUND)
 
     @Transactional
-    fun create(request: TodoRequest): Todo {
+    fun create(request: TodoRequest?): Todo {
         checkNotNull(request) { "TodoRequest is null" }
 
-        val todo = Todo.builder()
-            .title(request.title)
-            .description(request.description)
-            .done(false)
-            .createdAt(LocalDateTime.now())
-            .build()
+        val todo = Todo(
+            title = request.title,
+            description = request.description,
+            done = request.done,
+            createdAt = LocalDateTime.now(),
+        )
 
         return todoRepository.save(todo)
     }
 
     @Transactional
-    fun update(id: Long, request: TodoRequest) : Todo{
+    fun update(id: Long, request: TodoRequest?): Todo {
         checkNotNull(request) { "TodoRequest is null" }
 
-        val todo = findById(id)
-        todo.update(request.title, request.description, request.done)
-        return todo
+        return findById(id).let {
+            it.update(request.title, request.description, request.done)
+            return it
+        }
     }
 
-    fun delete(id: Long){
-        todoRepository.deleteById(id)
-    }
+    fun delete(id: Long) = todoRepository.deleteById(id)
+
 }
