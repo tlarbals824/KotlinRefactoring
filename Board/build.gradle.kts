47c7ff1249269dc681924f15983b62500e24df79
tlarbals824 <simkm0824@naver.com>
2023-12-19 21:16:46 +0900

    chore : kotest, swagger 의존성 추가

---
+++ Board/build.gradle.kts
@@ -0,0 +1,9 @@
+dependencies {
+    // kotest
+    testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.6.2")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+
+    // swagger
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+}
